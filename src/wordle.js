2deea3bd8b6b46800fa9f4a5e60adfbd6aec4586
etgoz <etgoz151@gmail.com>
2022-11-18 16:14:49 +0200

    js input letters and delete

--- src/wordle.js
+++ src/wordle.js
@@ -1,13 +1,25 @@
-const firstCell = document.querySelector('.input-zone input');
-const allInputs = document.querySelectorAll('.input-zone input');
+const firstCell = document.querySelector(".input-zone .input");
+const allInputs = document.querySelectorAll(".input-zone .input");
 
-document.addEventListener('DOMContentLoaded', () => {
-    firstCell.focus();
-})
+document.addEventListener("DOMContentLoaded", () => {
+	firstCell.focus();
+});
 
-Array.from(allInputs).forEach((input) => {
-    input.setAttribute('pattern', '[א-ת]');
-    addEventListener('oninput', () =>{
-        console.log(input.innerHTML);
-    })
+function contains_heb(str) {
+	return /[\u0590-\u05FF]/.test(str);
+}
+
+document.addEventListener("keydown", (ev) => {
+	const key = ev.key;
+	if (contains_heb(key)) {
+		document.querySelector(":focus").innerHTML = key;
+		document.querySelector(":focus").nextElementSibling.focus();
+	} else if (key === "Backspace" || key === "Delete") {
+		if (document.querySelector(":focus").innerHTML !== "") {
+			document.querySelector(":focus").innerHTML = "";
+		} else {
+			document.querySelector(":focus").previousElementSibling.focus();
+			document.querySelector(":focus").innerHTML = "";
+		}
+	}
 });
